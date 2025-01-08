7b315fe68d6d92151cc5148178bbe9bf8baa083a
redwanfahim <reedwaaan@gmail.com>
2025-01-08 17:49:32 +0600

    Ver 2025.1.0

--- code1.py
+++ code1.py
@@ -1,17 +1,12 @@
 '''first Code'''
 
+
 def greetings():
-    
+
     '''greeting message'''
     print("Hello World")
-    
     print("Hello World 2")
-    
     print("Rakib Gay")
 
 
-
 greetings()
-
-
-
